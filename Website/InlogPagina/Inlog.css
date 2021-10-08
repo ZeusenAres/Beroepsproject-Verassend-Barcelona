241c5de6b368fb4ac4b5874a3581f55c56616b33
Sophia Links <sophialinks@me.com>
2021-10-08 13:52:21 +0200

    Inlog

---
+++ Website/InlogPagina/Inlog.css
@@ -0,0 +1,57 @@
+* {
+    margin: 0;
+    padding: 0;
+}
+
+.mordor {
+    display: flex;
+    justify-content: center;
+}
+
+.container {
+    display: flex;
+    justify-content: center;
+    left: 18.59%;
+    right: 18.65%;
+    top: 15.56%;
+    bottom: 30.28%;
+    Width: 1000px;
+    height: 400px;
+    background: rgba(251, 199, 17, 0.19);
+    border-radius: 50px;
+    margin-top: 150px;
+}
+
+h1 {
+    font-family: Assistant;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 48px;
+    line-height: 63px;
+    color: #662C25;
+}
+
+#Button {
+    background-color: #7CC644;
+    color: #662C25;
+    border-radius: 50px;
+    left: 870px;
+}
+
+#Gebruikersnaam, #Wachtwoord, #Button {
+    width: 750px;
+    height: 50px;
+    border-radius: 10px;
+    margin-top: 15px;
+}
+
+    #Gebruikersnaam[type="text"], #Wachtwoord, #Button {
+        font-family: Assistant;
+        color: #662C25;
+        font-size: 25px;
+    }
+
+        #Button:hover {
+            background-position: 0 -135px;
+            color: #00aeef;
+        }
