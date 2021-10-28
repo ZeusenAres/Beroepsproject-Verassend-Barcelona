141eae3380cccf86fbbd5731373baa7e8087b1f8
ZeusenAres <2060537@talnet.nl>
2021-10-28 19:27:28 +0200

    Add project files.

---
+++ Website/Homepage/homepage.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../header-footer/header.footer.css">
+    <link rel="stylesheet" href="homepage.content.page.css">
+    <title>www.verassendbarcelona.nl</title>
+</head>
+<body>
+
+    <!-- Navbar -->
+    <nav class="navbar">
+        <div class="headcapsule">
+            <img class="logoimg" src="../IMG/Verassend_Barcelona_LOGO.png" alt="[LOGO]">
+            <h1 class="headertitle">Wij nemen je mee op een avontuur!</h1>
+
+
+            <div class="dropdown">
+                <button class="dropbtn">Menu</button>
+                <div class="dropdown-content">
+                    <a href="homepage.html">Home</a>
+                    <a href="....">Reserveren</a>
+                    <a href="....">Informatie</a>
+                    <a href="....">Prijzen</a>
+                </div>
+            </div>
+        </div>
+
+    </nav>
+
+
+
+    <div class="startcontainer">
+        <div class="containerA">
+            <h1 class="containertitle">Fiets-tours</h1>
+            <img src="../IMG/Fietstour-Voorblad.png"/>
+        </div>
+
+        <div class="containerB" id="left">
+            <h1 class="containertitle">Fiets-verhuur</h1>
+            <img src="../IMG/Fietshuur-Voorblad.png"/>
+        </div>
+    </div>
+    <!-- Footer -->
+
+    <div class="footercapsule">
+        <div class="footerlogo">
+            <img class="logofooter" src="../IMG/Verassend_Barcelona_Logo.png" alt="Logo">
+        </div>
+
+        <div class="textfooter">
+            <h1 class="footerhead">Fietsenhuren</h1>
+            <ul class="fietsen-footer">
+                <li>Fietsen</li>
+                <li>Fietstours</li>
+
+            </ul>
+        </div>
+
+        <div class="textfooter">
+            <h1 class="footerhead">Contact</h1>
+            <ul class="Contact-footer">
+                <li>
+                    Openingstijden: <br> (maandag t/m zaterdag van 09:00 tot 18:00 <br>
+                    zondag: Gesloten.)
+                </li>
+                <li>Tel: 020 569 696 61</li>
+                <li>E-mail: Info@VerassendBarcelona.com</li>
+                <li>
+                    <a href="https://www.google.nl/maps/dir//Carrer+de+Bergara,+69,+08002+Barcelona,+Spanje/
+                @41.3865754,2.1679125,19z/data=!4m8!4m7!1m0!1m5!1m1!1s0x12a4a2f38c88911d:0xf4c4ae83f9d207
+                db!2m2!1d2.1685425!2d41.3865368">
+                        Carrer de Bergara 69 <br>
+                        08007 DJ Barcelona
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="route">
+            <a href="https://www.google.nl/maps/dir//Carrer+de+Bergara,+69,+08002+Barcelona,+Spanje/
+            @41.3865754,2.1679125,19z/data=!4m8!4m7!1m0!1m5!1m1!1s0x12a4a2f38c88911d:0xf4c4ae83f9d207
+            db!2m2!1d2.1685425!2d41.3865368" target=_blank><input type="image" src="../IMG/route.png" id="route" name="route" alt="route"></a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ Website/Informatiepagina/informatiepagina.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Informatiepagina</title>
+    <link rel="stylesheet" href="informatiepagina.css">
+    <link rel="stylesheet" href="../header-footer/header.footer.css">
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar">
+        <div class="headcapsule">
+            <img class="logoimg" src="../IMG/Verassend_Barcelona_LOGO.png" alt="[LOGO]">
+            <h1 class="headertitle">Wij nemen je mee op een avontuur!</h1>
+
+
+            <div class="dropdown">
+                <button class="dropbtn">Menu</button>
+                <div class="dropdown-content">
+                    <a href="....">Home</a>
+                    <a href="....">Reserveren</a>
+                    <a href="....">Informatie</a>
+                    <a href="....">Prijzen</a>
+                </div>
+            </div>
+        </div>
+
+    </nav>
+    
+    <br>
+    <div class="fietsverhuur">
+    <div class="reserveren">
+    <h1>Reserveer een fiets online of in de winkel!</h1><br>
+    <p1>
+        Informatie fietsverhuur:<br>
+        De fietsen kunnen gereserveerd worden in tijdslots van 4 uur, zo heb jij alle vrijheid om zelf de stad te ontdekken!<br>
+        Er zijn maximaal 20 fietsen beschikbaar, zorg dus dat je online een tijdslot reserveerd om verzekerd te zijn van een fiets! (Fietsen voor de tours staan apart en zitten niet in de fietsverhuur.) De kosten voor het huren van een fiets bedraagt 15 euro per persoon per fiets.<br>
+        Verkent hot-spots in de stad zoals; Sagrada Familia, Las Ramblas, de Gothic Quarter en La Barceloneta! Alle populaire locaties zijn gemakkelijk met de fiets toegankelijk vanaf ons filiaal in Barcelona. <br>
+        Barcelona is dankzij de uitstekende infrasttructuur (o.a. honderden kilometers fietspad) naast leuk, informatief en ontspannen ook erg veilig! <br>
+        Word verrast door vriendelijk en behulpzaam personeel die allemaal over de Nederlandse taal beschikken en geniet van de comfort van onze Cortina City Bikes, aanbevolenn door de ANWB! <br>
+    </p1>
+        <img class="verhuurfoto" src="../IMG/fietsverhuur.png" alt="fietsverhuur">
+    <button type="button">Reserveer hier</button>
+    </div>
+    
+    <div class="fietstour">
+    <h2>Reserveer de Fiets-tour online of in de winkel!</h2><br>
+    <p2>
+        Informatie fiets-tour:<br>
+        De fiets-tour duurt 2,5 uur lang en word 2 keer per dag gehouden: <br>
+        Om 10.00 uur en 15.00 uur. <br>
+        Er kunnen maximaal 10 mensen mee per fiets-tour. (Fietsen voor deze tour staan apart en zitten niet in de losse fietsverhuur.) De kosten voor deze tour bedragen 10 euro per persoon per fiets. <br>
+        Incl. hoogtepunten als Sagrada Familia, Las Ramblas, de Gothic Quarter en La Barceloneta. Deze ontspannen Barcelona fiets-tour met een Nederlandse, lokale gids combineert in slechts 2,5 uur de belangrijkste must-see highlights; o.a. enkele meesterwerken van Gaudi, met de verborgen schatten van deze prachtige wereldstad. <br>
+        De Nederlandstalige local vertelt de complete historie en achtergronden, zodat je in korte tijd alles over deze topstad te weten komt! Deze Barcelona fiets-tour is dankzij de uitstekende infrastructuur (o.a. honderden kilometers fietspad) naast leuk, informatief en ontspannen ook 100% veilig. <br>
+        Met deze Barcelona fiets-tour, die inzoomt op de niet te missen hoogtepunten. Deze hoogtepunten ga je zien tijdens de nr. 1 Barcelona fiets-tour.
+    </p2>
+        <img class="tourfoto" src="../IMG/fietstour.png" alt="fietstour">
+    <button type="button">Reserveer hier</button>
+    </div>
+        </div>
+
+    <!-- Footer -->
+
+    <div class="footercapsule">
+        <div class="footerlogo">
+            <img class="logofooter" src="../IMG/Verassend_Barcelona_Logo.png" alt="Logo">
+        </div>
+
+        <div class="textfooter">
+            <h1 class="footerhead">Fietsenhuren</h1>
+            <ul class="fietsen-footer">
+                <li>Fietsen</li>
+                <li>Fietstours</li>
+
+            </ul>
+        </div>
+
+        <div class="textfooter">
+            <h1 class="footerhead">Contact</h1>
+            <ul class="Contact-footer">
+                <li>
+                    Openingstijden: <br> (maandag t/m zaterdag van 09:00 tot 18:00 <br>
+                    zondag: Gesloten.)
+                </li>
+                <li>Tel: 020 569 696 61</li>
+                <li>E-mail: Info@VerassendBarcelona.com</li>
+                <li>
+                    <a href="https://www.google.nl/maps/dir//Carrer+de+Bergara,+69,+08002+Barcelona,+Spanje/
+                @41.3865754,2.1679125,19z/data=!4m8!4m7!1m0!1m5!1m1!1s0x12a4a2f38c88911d:0xf4c4ae83f9d207
+                db!2m2!1d2.1685425!2d41.3865368">
+                        Carrer de Bergara 69 <br>
+                        08007 DJ Barcelona
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="route">
+            <a href="https://www.google.nl/maps/dir//Carrer+de+Bergara,+69,+08002+Barcelona,+Spanje/
+            @41.3865754,2.1679125,19z/data=!4m8!4m7!1m0!1m5!1m1!1s0x12a4a2f38c88911d:0xf4c4ae83f9d207
+            db!2m2!1d2.1685425!2d41.3865368" target=_blank><input type="image" src="../IMG/route.png" id="route" name="route" alt="route"></a>
+        </div>
+    </div>
+
+
+    </div>
+</body>
+</html>
